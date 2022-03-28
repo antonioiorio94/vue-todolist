f2f15434cf9cc137af4aadafd064dba2f24f539b
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-28 14:43:53 +0200

    aggiunta array oggetti

--- script/main.js
+++ script/main.js
@@ -1,1 +1,25 @@
-console.log("Js OK");
+/*
+Descrizione:
+Rifare l'esercizio della to do list.
+Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+- text, una stringa che indica il testo del todo
+- done, un booleano (true/false) che indica se il todo è stato fatto oppure no*/
+
+//inserire in data un array di oggetti che hanno come propietà text e done
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    toDoItem: [
+      { text: "Fare colazione", done: true },
+
+      { text: "Seguire la lezione", done: true },
+
+      { text: "Prendere un caffè", done: false },
+
+      { text: "Seguire la spiegazione dell'esercizio", done: true },
+
+      { text: "Preparare il pranzo", done: false },
+    ],
+  },
+});
